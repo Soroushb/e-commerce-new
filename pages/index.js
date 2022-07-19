918ddcb209773329575c587dda707daf37e09f9a
Soroush Bahrami <sbahrami7@myseneca.ca>
2022-07-19 17:12:19 -0400

    Sanity Hooked and Data displayed

--- pages/index.js
+++ pages/index.js
@@ -1,20 +1,34 @@
 import React from 'react'
 import { Product, FooterBanner, HeroBanner } from '../components'
+import { client } from '../lib/client';
 
-const Home = () => {
+const Home = ({products, bannerData}) => {
   return (<>
-  <HeroBanner/>
+  <HeroBanner heroBanner={bannerData.length && bannerData[0]}/>
 
     <div className='products-heading'>
       <h2>Recommended</h2>
       <p>Second-hand Classic Books in Great Condition</p>
     </div>
     <div>
-      {['product 1', 'Product 2'].map((product) => product)}
+      {products?.map((product) => product.name)}
     </div>
     <FooterBanner/>
     </>
   )
 }
 
+export const getServerSideProps = async () => {
+
+  const query = '*[_type == "product"]';
+  const products = await client.fetch(query);
+
+  const bannerQuery = '*[_type == "banner"]';
+  const bannerData = await client.fetch(bannerQuery);
+
+  return{
+    props: {products, bannerData}
+  }
+
+}
 export default Home
