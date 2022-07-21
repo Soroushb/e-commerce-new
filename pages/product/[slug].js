3f4b3cacb54c5f1d21f6036fd43261db2b118038
Soroush Bahrami <sbahrami7@myseneca.ca>
2022-07-21 13:24:11 -0400

    StateContext Created and Decrease and Increase Qty buttons work

--- pages/product/[slug].js
+++ pages/product/[slug].js
@@ -2,10 +2,12 @@ import React, {useState} from 'react'
 import { urlFor, client } from '../../lib/client'
 import { AiOutlineMinus, AiOutlinePlus, AiFillStar, AiOutlineStar } from 'react-icons/ai';
 import { Product } from '../../components';
+import { useStateContext } from '../../context/StateContext'
 
 const ProductDetails = ({product, products}) => {
     const {image, name, details, price} = product;
     const [index, setIndex] = useState(0)
+    const {incQty, decQty, qty} = useStateContext();
 
 
   return (
@@ -47,9 +49,9 @@ const ProductDetails = ({product, products}) => {
                 <div className='quantity'>
                   <h3>Quantity:</h3>
                   <p className='quantity-desc'>
-                    <span className='minus' onClick=""><AiOutlineMinus/></span>
-                    <span className='num' onClick="">0</span>
-                    <span className='plus' onClick=""><AiOutlinePlus/></span>
+                    <span className='minus' onClick={decQty}><AiOutlineMinus/></span>
+                    <span className='num' onClick="">{qty}</span>
+                    <span className='plus' onClick={incQty}><AiOutlinePlus/></span>
                   </p>
                 </div>
                 <div className='buttons'>
