4bfa98b750b1171ed09aecd66decc9c1f36faa51
Soroush Bahrami <sbahrami7@myseneca.ca>
2022-07-22 16:46:58 -0400

    Minor details onAdd

--- context/StateContext.js
+++ context/StateContext.js
@@ -10,6 +10,31 @@ export const StateContext = ({children}) => {
     const [totalPrice, setTotalPrice] = useState();
     const [totalQuantities, setTotalQuantities] = useState();
     const [qty, setQty] = useState(1);
+
+    const onAdd = (product, quantity) => {
+        
+        const checkProductInCart = cartItems.find((item) => item._id === product._id)
+
+        setTotalPrice((prevTotalPrice) => prevTotalPrice + (product.price * quantity ))
+        setTotalQuantities((prevTotalQuantites) => prevTotalQuantites + quantity)
+
+        if(checkProductInCart) {
+          
+            const updatedCartItems = cartItems.map((cartProduct) => {
+                if (cartProduct._id === product._id){
+                    return {...cartProduct,
+                    quantity: cartProduct.quantity + quantity}
+
+                }
+            })
+
+            setCartItems(updatedCartItems);
+            toast.success(`${qty} ${product.name} added to the cart.`);
+        }else{
+
+        }
+    }
+
     const incQty = () => {
         setQty((prevQty) => prevQty + 1)
     }
