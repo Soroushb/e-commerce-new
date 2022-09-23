32421a77519f23c92255e03fb85ca13d18465097
Soroush Bahrami <sbahrami41@yahoo.com>
2022-09-23 19:23:23 -0400

    price filter place-holders added

--- components/Footer.jsx
+++ components/Footer.jsx
@@ -1,12 +1,16 @@
 import React from 'react'
 import { AiFillLinkedin  } from 'react-icons/ai'
-
+import Link from 'next/link'
 
 const Footer = () => {
   return (
     <div className='footer-container'>
       <p>2022 Soroush's Bookshop All rights reserved.</p>
-      <p className='icons'><AiFillLinkedin/></p>
+      <Link href="https://www.linkedin.com/in/soroush-bahrami-ba691b19b/" >
+        <a target="_blank">
+        <AiFillLinkedin className='icons'/>
+        </a>
+        </Link>
     </div>
   )
 }
